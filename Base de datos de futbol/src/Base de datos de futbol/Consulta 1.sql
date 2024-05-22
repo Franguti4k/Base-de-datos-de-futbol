cb9fea4f2f5a2e93e738da0c652847f00be34c1f
Francisco Javier Gutiérrez Gallego <114072492+Franguti4k@users.noreply.github.com>
2024-05-22 13:17:28 +0200

    Delete Base de datos de futbol/src directory

--- Base de datos de futbol/src/Base de datos de futbol/Consulta 1.sql
+++
@@ -1,7 +0,0 @@
-/*SENTENCIA Nº1
-Mostramos a la liga; la temporada en la que se jugó,
-si es a partir de la temporada 2020-21; y quién gano la competicion
-*/
-SELECT l.nombre AS Competicion, t.temporada, e.nombre AS Equipo_Ganador FROM equipo e, liga l, participaciones t
-WHERE t.temporada LIKE '202%' AND e.idEquipo=t.idEquipo AND l.idLiga=t.idLiga AND t.posicion < 2
-ORDER BY l.nombre ASC, t.temporada ASC, t.posicion ASC;
--- Base de datos de futbol/src/Base de datos de futbol/Consulta 2.sql
+++
@@ -1,8 +0,0 @@
-/*SENTENCIA Nº2
-Mostrar las veces que un equipo ha ganado una competición, siempre y cuando lo haya 5 veces o más
-*/
-SELECT l.nombre, e.nombre, count(*) AS Veces_Ganada FROM equipo e, liga l, participaciones p
-WHERE e.idEquipo=p.idEquipo AND l.idLiga=p.idLiga AND p.posicion=1
-GROUP BY e.idEquipo, l.idLiga
-HAVING Veces_Ganada >= 5
-ORDER BY l.nombre ASC,Veces_Ganada DESC;
